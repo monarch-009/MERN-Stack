110b2d803af61bf4078551fd02db66fe40a6f810
monarch-009 <adityasinghmoni@gmail.com>
2023-12-21 14:48:29 +0530

    used sillyname and superheros package

--- Node/First_Node_Project/index.js
+++ Node/First_Node_Project/index.js
@@ -1,6 +1,15 @@
 // First time using Node.js
 //-> node index.js
 
-let sillyname = require('sillyname');
-let name = sillyname();
-console.log(`My name is ${name}`);
+//let generateName = require('sillyname');
+
+// import genrateName from 'sillyname';
+
+// let name = genrateName();
+// console.log(`My name is ${name}`);
+
+
+import superheros from 'superheroes';
+
+let mySuperHeroName = superheros.random();
+console.log(`My Super Hero name is ${mySuperHeroName}`);
