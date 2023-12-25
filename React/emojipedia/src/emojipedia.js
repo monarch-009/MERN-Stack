4cd9d7b07cc2f36cbe05b93c0112aa6742b6102b
monarch-009 <adityasinghmoni@gmail.com>
2023-12-25 16:37:22 +0530

    React mapping project

---
+++ React/emojipedia/src/emojipedia.js
@@ -0,0 +1,46 @@
+const emojipedia = [
+  {
+    id: 1,
+    emoji: "💪",
+    name: "Tense Biceps",
+    meaning:
+      "“You can do that!” or “I feel strong!” Arm with tense biceps. Also used in connection with doing sports, e.g. at the gym.",
+  },
+  {
+    id: 2,
+    emoji: "🙏",
+    name: "Person With Folded Hands",
+    meaning:
+      "Two hands pressed together. Is currently very introverted, saying a prayer, or hoping for enlightenment. Is also used as a “high five” or to say thank you.",
+  },
+  {
+    id: 3,
+    emoji: "🤣",
+    name: "Rolling On The Floor, Laughing",
+    meaning:
+      "This is funny! A smiley face, rolling on the floor, laughing. The face is laughing boundlessly. The emoji version of “rofl“. Stands for „rolling on the floor, laughing“.",
+  },
+  {
+    id: 4,
+    emoji: "😘",
+    name: "Face Blowing a Kiss",
+    meaning:
+      "Smiley sends you a loving kiss that comes from the heart. The kiss is meant to say “I like you” or “You’re the best!”. Loving kiss to the closest friends, family or darling. Smiley likes you and wants to cuddle up or hold hands.",
+  },
+  {
+    id: 5,
+    emoji: "😷",
+    name: "Face with Medical Mask",
+    meaning:
+      "I am ill or I am afraid of getting sick. People wear the mask to avoid getting infected by bacteria and viruses or to protect others.",
+  },
+  {
+    id: 6,
+    emoji: "😍",
+    name: "Smiling Face with Heart-Eyes",
+    meaning:
+      "I love it or I am in love. The face with hearts is used especially frequently in the digital love. It stands for desire, love and adoration.",
+  }
+];
+
+export default emojipedia;
